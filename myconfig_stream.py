723ca9efb1c8ae62c9050bd0ff5e189c2c52f53c
Brandon Forys <bforys@gmail.com>
2018-07-18 16:36:14 -0700

    Add files via upload

---
+++ myconfig_stream.py
@@ -0,0 +1,15 @@
+# Analysis Network parameters:
+
+scorer = 'Rene'
+Task = 'side_right_paw_movement'
+date = 'June12_18'
+trainingsFraction = 0.95  # Fraction of labeled images used for training
+resnet = 50
+snapshotindex = -1
+shuffle = 1
+
+# For plotting:
+trainingsiterations = 2000  # type the number listed in .pickle file
+pcutoff = 0.1  # likelihood cutoff for body part in image
+# delete individual (labeled) frames after making video?
+deleteindividualframes = False
